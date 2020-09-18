e2f509480b5cd78da5c368947269164db254a401
Belov Roman <belov.rom-77@yandex.ru>
2020-09-18 14:18:42 +0300

    Заменяет babel/polyfill на corejs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,66 +1,60 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle-[hash].js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000,
-        watchContentBase: true,
-        progress: true
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(sass|scss|css)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === 'development',
-                            publicPath: './dist/styles',
-                            reloadAll: true,
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|webp)$/,
-                use: ['file-loader'],
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            inject: false,
-            template: require('html-webpack-template'),
-            title: 'Document',
-            mobile: true,
-            lang: 'en-US',
-            appMountId: 'root'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name]-[hash].css'
-        })
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle-[hash].js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif|webp)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      }
     ]
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require("html-webpack-template"),
+      title: "Document",
+      mobile: true,
+      lang: "en-US",
+      appMountId: "root"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]-[hash].css"
+    })
+  ]
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,79 +1,81 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle-[hash].js'
-    },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(sass|scss|css)$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === 'development',
-                            publicPath: './dist/styles',
-                            reloadAll: true,
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|webp)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192,
-                    },
-                }, 'image-webpack-loader'],
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true,
-                    }
-                }]
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle-[hash].js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif|webp)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
             }
+          },
+          "image-webpack-loader"
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            inject: false,
-            template: require('html-webpack-template'),
-            title: 'Document',
-            mobile: true,
-            lang: 'en-US',
-            appMountId: 'root'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name]-[hash].css'
-        })
-    ],
-    optimization: {
-        minimizer: [
-            new OptimizeCssPlugin(),
-            new TerserPlugin({
-                cache: true,
-                parallel: true,
-            }),
-        ],
-    }
-}
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require("html-webpack-template"),
+      title: "Document",
+      mobile: true,
+      lang: "en-US",
+      appMountId: "root",
+      minify: {
+        collapseWhitespace: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]-[hash].css"
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCssPlugin(),
+      new TerserPlugin({
+        cache: true,
+        parallel: true
+      })
+    ]
+  }
+};
