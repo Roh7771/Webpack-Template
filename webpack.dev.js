f40b08f0badd7ed22d5d16aeb495ed93dd4f32ea
Roman Belov <belov.rom-77@yandex.ru>
2020-09-20 17:37:12 +0300

    Редактирует стиль кода

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,56 +1,56 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: ['./src/index.js'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle-[hash].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle-[hash].js',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
     watchContentBase: true,
-    progress: true
+    progress: true,
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.(sass|scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|gif|webp)$/,
-        use: ["file-loader"]
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        use: ['file-loader'],
       },
       {
         test: /\.html$/,
-        use: ["html-loader"]
-      }
-    ]
+        use: ['html-loader'],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html"
+      filename: 'index.html',
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name]-[hash].css"
-    })
-  ]
+      filename: '[name]-[hash].css',
+    }),
+  ],
 };
